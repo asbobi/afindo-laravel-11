35525a31041673c1a15b6451bb3d68f3a3236cdd
Asbobi <asbobimulyo567@gmail.com>
2025-01-16 15:06:30 +0700

    move css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -45,6 +45,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css"
     integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<link rel="stylesheet" href="{{ asset('app-assets/css/afindo.css') }}">
 <!-- END: Page CSS-->
 
 <!-- BEGIN: Custom CSS-->
